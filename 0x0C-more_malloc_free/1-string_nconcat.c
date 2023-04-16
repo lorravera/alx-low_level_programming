0619ac00ed04c5dd40b2a5566bb3e9f147a871c1
lorravera <lorrainevera50@gmail.com>
2023-04-16 13:55:32 -0700

    more

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -5,21 +5,21 @@
  * *string_nconcat -concatenates n bytes of a string to another string
  * @s1: string to append to
  * @s2: string to concatenate from
- * @n: number of bytes from s2 to concatenate to s1
+ * @i: number of bytes from s2 to concatenate to s1
  *
  * Return: pointer to the resulting string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int i)
-{       
-        char *t;
-        unsigned int j = 0, k = 0, len1 = 0, len2 = 0;
-        
-        while (s1 && s1[len1])
-                len1++;
-        while (s2 && s2[len2])
-                len2++;
-        
-        if (i < len2)
+{
+	char *t;
+	unsigned int j = 0, k = 0, len1 = 0, len2 = 0;
+
+	while (s1 && s1[len1])
+	len1++;
+	while (s2 && s2[len2])
+	len2++;
+
+	if (i < len2)
 		t = malloc(sizeof(char) * (len1 + i + 1));
 	else
 		t = malloc(sizeof(char) * (len1 + len2 + 1));
@@ -37,7 +37,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int i)
 		t[i++] = s2[k++];
 
 	while (i < len2 && i < (len1 + len2))
-                t[i++] = s2[k++];
+		t[i++] = s2[k++];
 
 	t[j] = '\0';
 
