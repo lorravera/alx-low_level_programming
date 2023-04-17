da708cd13356f5908511ddf336bb5b6c8e2110df
lorravera <lorrainevera50@gmail.com>
2023-04-17 12:39:03 -0700

    preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -4,7 +4,7 @@
  * main - prints the name of
  * the file it was compiled from
  *
- * Return: null
+ * Return: nothing
  */
 
 int main(void)
--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) (((x) < (0)) ? ((X) * (-1)) : (x))
+#define ABS(x) (((X) < (0)) ? ((X) * (-1)) : (X))
 
 #endif
